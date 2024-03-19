a03624065473a26943ee8fea8f643eb711971308
Syed Maaz Ullah Shah <su06942@st.habib.edu.pk>
2024-03-19 13:56:28 +0500

    change heading

--- frontend/App.js
+++ frontend/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>SANATKADA</Text>
+      <Text>your craft store</Text>
       <StatusBar style="auto" />
     </View>
   );
