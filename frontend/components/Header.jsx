4699fb1b92f51e95052d864e7de516919e5643d7
Syed Maaz Ullah Shah <su06942@st.habib.edu.pk>
2024-03-22 23:45:07 +0500

    Add categories component and Search Button

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,12 +1,13 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Avatar } from 'react-native-paper'
 import { colors } from '../styles/styles'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 
-const Header = ({back, emptyCart=false}) => {
+const Header = ({ back, emptyCart=false }) => {
 
     const navigate = useNavigation();
+    const route = useRoute()
     const emptyCartHandler = () => {
         console.log("Empty Cart")
     }
@@ -26,7 +27,7 @@ const Header = ({back, emptyCart=false}) => {
                 <Avatar.Icon 
                 style={{ backgroundColor: colors.color4 }}
                 icon={"arrow-left"}
-                color={colors.color3} 
+                color={route.name==="productdetails" ? colors.color2  : colors.color3} 
                 />
             </TouchableOpacity>
         )
@@ -43,7 +44,7 @@ const Header = ({back, emptyCart=false}) => {
             <Avatar.Icon 
             style={{ backgroundColor: colors.color4 }}
             icon={emptyCart ? "delete-outline" : "cart-outline"}
-            color={colors.color3} 
+            color={route.name==="productdetails" ? colors.color2  : colors.color3} 
             />
     </TouchableOpacity>
 
--- frontend/screens/Home.jsx
+++ frontend/screens/Home.jsx
@@ -1,15 +1,41 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
-import { defaultStyle } from '../styles/styles';
+import { colors, defaultStyle } from '../styles/styles';
 import Header from '../components/Header';
+import { Avatar, Button } from 'react-native-paper';
 
 const Home = () => {
   return (
     <View style = {defaultStyle}>
         <Header/>
-        <View>
-            <Text style = {{ fontSize:25, fontWeight:"300" }}>Our</Text>    
-            <Text style = {{ fontSize:25, fontWeight:"500" }}>Products</Text>
+
+        <View style={{
+            paddingTop: 55,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center"
+        }}>
+
+            <View>
+                <Text style = {{ fontSize:25, fontWeight:"300" }}>Our</Text>    
+                <Text style = {{ fontSize:25, fontWeight:"500" }}>Products</Text>
+            </View> 
+
+            <View>
+                <TouchableOpacity>
+                    <Avatar.Icon 
+                    icon={"magnify"}
+                    size={50} 
+                    color={"gray"} 
+                    style={{ backgroundColor:colors.color2, elevation: 12}} />
+                </TouchableOpacity>
+            </View>
+        </View>
+
+        <View style={{flexDirection: "row", height:80}}>
+            <Button style={{backgroundColor: colors.color5, borderRadius: 100, margin: 5}}>
+                <Text style={{fontSize: 12, color: "gray"}}>Categories</Text>
+            </Button>
         </View>
 
     </View>
