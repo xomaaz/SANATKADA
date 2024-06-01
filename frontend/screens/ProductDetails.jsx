73344e8f2720378ca53b3276cb6c0f36639d9004
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-06-01 12:45:40 +0500

    Add ProductDetails screen (some styling still needed)

--- frontend/screens/ProductDetails.jsx
+++ frontend/screens/ProductDetails.jsx
@@ -1,15 +1,32 @@
-import React, { useRef } from 'react';
-import { View, Dimensions, StyleSheet, Image } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, Text, Dimensions, StyleSheet, Image, Touchable, TouchableOpacity } from 'react-native';
 import { colors, defaultStyle } from '../styles/styles';
 import Header from '../components/Header';
 import Carousel from 'react-native-snap-carousel';
+import { Avatar } from 'react-native-paper';
 
 const SLIDER_WIDTH = Dimensions.get('window').width;
 const ITEM_WIDTH = SLIDER_WIDTH;
+const iconOptions = {
+    size: 20,
+    style: {
+        borderRadius: 5,
+        backgroundColor: colors.color5,
+        height: 25,
+        width: 25,
+    }
+}
 
 const ProductDetails = ( { route: { params } } ) => {
     console.log(params.id);
     const isCarousel = useRef();
+    const [quantity, setQuantity] = useState(1);
+
+    const name = "Yeezy";
+    const price = "500"; 
+    const description = "The YEEZY BOOST 350 V2features an upper composed of re-engineered Primeknit. The post-dyed monofilament side stripe is woven into the upper. Reflective threads are woven into the laces. The midsole utilizes adidas’ innovative BOOST™ technology.";
+    const stock = 5; // temporary stock amount
+
     const images = [
         {
             id: "001",
@@ -20,6 +37,16 @@ const ProductDetails = ( { route: { params } } ) => {
             url: "https://upload.wikimedia.org/wikipedia/commons/7/72/Adidas_Yeezy_Boost_350_Pirate_Black.gif",
         },
     ];
+
+    const incrementQty = () => {
+        if(stock <= quantity) return; // if stock is <= quantity, do nothing and return the same quantity
+        setQuantity((prev) => prev + 1);
+    };
+
+    const decrementQty = () => {
+        if(quantity <= 1) return; // if quantity is <= 1, do nothing and return the same quantity
+        setQuantity((prev) => prev - 1);
+    };
 
     return (
         <View
@@ -39,6 +66,57 @@ const ProductDetails = ( { route: { params } } ) => {
                 data = {images}
                 renderItem = {CarouselCardItem}
             />
+
+            <View style={{
+                backgroundColor: colors.color2,
+                padding: 35,
+                flex: 1,
+                marginTop: -380,
+                borderTopLeftRadius: 55,
+                borderTopRightRadius: 55,
+            }}>
+                <Text numberOfLines={2} style={{ fontSize: 25, }}>{name}</Text>
+                <Text style={{ fontSize: 18, fontWeight: "900" }}>${price}</Text>
+                <Text numberOfLines={8} style={{ letterSpacing: 1, lineHeight: 20, marginVertical: 15, }}>{description}</Text>
+            </View>
+
+            <View style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingHorizontal: 5
+            }}>
+                <Text style={{
+                    colors: colors.color3,
+                    fontWeight: "100",
+                }}>Quantity</Text>
+            </View>
+
+            <View style={{
+                width: 80,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+            }}>
+                <TouchableOpacity onPress={decrementQty}>
+                    <Avatar.Icon 
+                        icon={'minus'}
+                        {...iconOptions} 
+                    />
+                </TouchableOpacity>
+                <Text style={styles.quantityStyle}>
+                    {quantity}
+                </Text>
+
+                <TouchableOpacity onPress={incrementQty}>
+                    <Avatar.Icon 
+                        icon={'plus'}
+                        {...iconOptions} 
+                    />
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     );
 };
@@ -60,6 +138,16 @@ const styles = StyleSheet.create({
         width: ITEM_WIDTH,
         resizeMode: "contain",
         height: 250,
+    },
+    quantityStyle: {
+        backgroundColor: colors.color4,
+        height: 25,
+        width: 25,
+        textAlignVertical: "center",
+        textAlign: "center",
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: colors.color5,
     },
 });
 
