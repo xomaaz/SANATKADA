5ed353eccc235508d66920335dd1fa2a1388bc04
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-06-02 08:01:45 +0500

    Add payment screen

--- frontend/Main.jsx
+++ frontend/Main.jsx
@@ -6,6 +6,7 @@ import ProductDetails from './screens/ProductDetails';
 import Toast from 'react-native-toast-message';
 import Cart from './screens/Cart';
 import ConfirmOrder from './screens/ConfirmOrder';
+import Payment from './screens/Payment';
 
 const Stack = createNativeStackNavigator();
 
@@ -23,6 +24,7 @@ const Main = () => {
         <Stack.Screen name="productdetails" component={ProductDetails} />
         <Stack.Screen name="Cart" component={Cart} />
         <Stack.Screen name="confirmorder" component={ConfirmOrder} />
+        <Stack.Screen name="payment" component={Payment} />
       </Stack.Group>
     </Stack.Navigator>
 
