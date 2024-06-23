60619841747239d0abf5cb035ecf05034ce03a9d
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-06-23 18:56:06 +0500

    Add user schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,50 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
-const schema = new mongoose.Schema({});
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter email"],
+    unique: [true, "Email already exists"],
+    validate: validator.isEmail,
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter password"],
+    minLength: [6, "Password must be at least 6 characters long"],
+    select: false, // prevents password from being included in a fetch request
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  pincode: {
+    type: Number,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  avatar: {
+    public_id: String,
+    url: String,
+  },
+  otp: Number,
+  otp_expire: Date,
+});
 
 export const User = mongoose.model("User", schema);
