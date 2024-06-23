53f337fced8dbd336e32ad700ed368e5f2dea447
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-06-23 18:02:14 +0500

    Connect MongoDB Atlas database to backend

--- server/data/database.js
+++ server/data/database.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGO_URI);
+    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "DB1",
+    });
 
-    console.log(`Server connected to database ${connection.host.name}`);
+    console.log(`Server connected to database ${connection.host}`);
   } catch (error) {
     console.log("Some error occured", error);
     process.exit(1);
