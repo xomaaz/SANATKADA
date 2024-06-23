fba34a989cac888e37483bfb5de9454e3b82718d
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-06-23 15:02:21 +0500

    Store /user url message handler in a controller variable and call it in user.js to display the message

---
+++ server/controllers/userController.js
@@ -0,0 +1,3 @@
+export const getMyProfile = (req,res,next) => { 
+  res.send("ME")
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,9 @@
 import express from "express";
+import { getMyProfile } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.route("/me").get((req,res,next) => { //here router.route("/me") stands for http://localhost:5000/api/v1/user/me
-  res.send("ME")
-})
+router.route("/me").get(getMyProfile) //here router.route("/me") stands for http://localhost:5000/api/v1/user/me
 ;
-
+ 
 export default router;
