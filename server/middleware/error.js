b759a54af6d0c165e25ed7d9a696936d2b195084
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-06-25 08:44:21 +0500

    Add custom error handler to handle duplicate values

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -2,6 +2,11 @@ export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
+  if (err.code == 11000) { // if entered value is duplicate
+    err.message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err.statusCode = 400;
+  }
+
   res.status(err.statusCode).json({ success: false, message: err.message });
 };
 
