d80f53ee1cc74ff9926984f77d544788f9424bc6
AliNisar00 <syed.alinisar2002@gmail.com>
2024-06-26 19:56:53 +0500

    Add cookie options: secure, HttpOnly, SameSite

--- server/utils/features.js
+++ server/utils/features.js
@@ -4,10 +4,16 @@ export const sendToken = (user, res, message, statusCode) => {
   res
     .status(statusCode)
     .cookie("token", token, {
-      secure: process.env.NODE_ENV === "production" ? true : false,
+      ...cookieOptions,
       expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000), // the cookie expires in 15 days (expressed in milliseconds)
     }).json({
       success: true,
       message: `Welcome Back, ${user.name}`,
   });
-}
+};
+
+export const cookieOptions = {
+  secure: process.env.NODE_ENV === "Development" ? false : true,
+  httpOnly: process.env.NODE_ENV === "Development" ? false : true,
+  sameSite: process.env.NODE_ENV === "Development" ? false : "none",
+};
