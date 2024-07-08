5927a53c2a48d2b26ade69f1d2af1212a0352917
Ali Nisar <64202928+AliNisar00@users.noreply.github.com>
2024-07-08 11:37:44 +0500

    Add "Create New Product" route to Product API

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,8 @@
 import { asyncError } from "../middleware/error.js";
 import { Product } from "../models/productModel.js";
 import ErrorHandler from "../utils/errorHandler.js";
+import { getDataUri } from "../utils/features.js";
+import cloudinary from "cloudinary";
 
 export const getAllProducts = asyncError(async (req, res, next) => {
   // Search & Category query (to be done later)
@@ -20,5 +22,32 @@ export const getProductDetails = asyncError(async (req, res, next) => {
   res.status(200).json({
     success: true,
     product,
+  });
+});
+
+export const createProduct = asyncError(async (req, res, next) => {
+  const { name, description, category, price, stock } = req.body;
+
+  if(!req.file) return next(new ErrorHandler("Please add image", 400));
+
+  const file = getDataUri(req.file); // get the buffer data from image
+  const myCloud = await cloudinary.v2.uploader.upload(file.content); // upload new avatar image
+  const image = { // store avatar details as an object in database
+    public_id: myCloud.public_id,
+    url: myCloud.secure_url,
+  };
+
+  await Product.create({
+    name,
+    description,
+    category,
+    price,
+    stock,
+    images: [image],
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Product Created Successfully",
   });
 });
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { isAuthenticated } from "../middleware/auth.js";
 import { singleUpload } from "../middleware/multer.js";
-import { getAllProducts, getProductDetails } from "../controllers/productController.js";
+import { createProduct, getAllProducts, getProductDetails } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.get("/all", getAllProducts);
 
 router.route("/single/:id").get(getProductDetails);
+
+router.post("/new", isAuthenticated, singleUpload, createProduct);
 
 export default router;
