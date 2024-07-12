d075bb3a2bdf1daa449fc480ca8941ae4cb19c86
AliNisar00 <syed.alinisar2002@gmail.com>
2024-07-12 08:33:29 +0500

    Add "getAdminOrders", "getOrderDetails", and "processOrder" routes to Order API

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,6 +1,7 @@
 import { asyncError } from "../middleware/error.js";
 import { Order } from "../models/orderModel.js";
 import { Product } from "../models/productModel.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 export const createOrder = asyncError(async(req, res, next) => {
   const { shippingInfo, orderItems, paymentMethod, paymentInfo, itemsPrice, taxPrice, shippingCharges, totalAmount } = req.body;
@@ -36,5 +37,45 @@ export const getMyOrders = asyncError(async(req, res, next) => {
   res.status(200).json({
     success: true,
     orders,
+  });
+});
+
+export const getAdminOrders = asyncError(async(req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+export const getOrderDetails = asyncError(async(req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) return next(new ErrorHandler("Order Not Found", 404));
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+export const processOrder = asyncError(async(req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) return next(new ErrorHandler("Order Not Found", 400));
+
+  if (order.orderStatus == "Preparing") order.orderStatus = "Shipped";
+  else if (order.orderStatus == "Shipped") {
+    order.orderStatus = "Delivered";
+    order.deliveredAt = new Date(Date.now());
+  }
+  else return next(new ErrorHandler("Order Already Delivered", 400));
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Order Processed Successfully",
   });
 });
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { createOrder, getMyOrders } from "../controllers/orderController.js";
-import { isAuthenticated } from "../middleware/auth.js";
+import { createOrder, getAdminOrders, getMyOrders, getOrderDetails, processOrder } from "../controllers/orderController.js";
+import { isAdmin, isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/new", isAuthenticated, createOrder);
 
 router.get("/my", isAuthenticated, getMyOrders);
+
+router.get("/admin", isAuthenticated, isAdmin, getAdminOrders);
+
+router.route("/single/:id").get(isAuthenticated, getOrderDetails).put(isAuthenticated, isAdmin, processOrder);
 
 export default router;
