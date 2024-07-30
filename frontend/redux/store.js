e69b48f96d73a0907c43f600937d25812af27c9e
AliNisar00 <syed.alinisar2002@gmail.com>
2024-07-30 20:02:16 +0500

    Create user reducer

--- frontend/redux/store.js
+++ frontend/redux/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { userReducer } from "./reducers/userReducer";
+import { userReducer } from "./reducers/userReducer";
 
 export const store = configureStore({
   reducer: {
-    // user: userReducer,
+    user: userReducer,
   },
 });
 
